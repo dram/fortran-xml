b50e6a9723ca7004070dd156bc5f9cbf564cce6c
Xin Wang <dram.wang@gmail.com>
2018-04-16 21:38:25 +0800

    Use pointer instead of character array

--- sources/xml.f90
+++ sources/xml.f90
@@ -6,10 +6,9 @@ module xml
      !! ********
      function xml_encode_entities_reentrant(doc, input) &
           bind(c, name="xmlEncodeEntitiesReentrant")
-       use iso_c_binding, only: c_char, c_ptr
-       type(c_ptr), value :: doc
-       character(kind=c_char), intent(in) :: input (*)
-       type(c_ptr) :: xml_encode_entities_reentrant
+       use iso_c_binding, only: c_ptr
+       type(c_ptr), value :: doc, input
+       type(c_ptr) xml_encode_entities_reentrant
      end function xml_encode_entities_reentrant
 
      !! tree
@@ -24,19 +23,16 @@ module xml
 
      function xml_save_file(filename, doc) &
           bind(c, name="xmlSaveFile")
-       use iso_c_binding, only: c_char, c_int, c_ptr
-       character(kind=c_char), intent(in) :: filename (*)
-       type(c_ptr), value :: doc
-       integer(c_int) :: xml_save_file
+       use iso_c_binding, only: c_int, c_ptr
+       type(c_ptr), value :: filename, doc
+       integer(c_int) xml_save_file
      end function xml_save_file
 
      function xml_save_file_enc(filename, doc, encoding) &
           bind(c, name="xmlSaveFileEnc")
-       use iso_c_binding, only: c_char, c_int, c_ptr
-       character(kind=c_char), intent(in) :: filename (*)
-       type(c_ptr), value :: doc
-       character(kind=c_char), intent(in) :: encoding (*)
-       integer(c_int) :: xml_save_file_enc
+       use iso_c_binding, only: c_int, c_ptr
+       type(c_ptr), value :: filename, doc, encoding
+       integer(c_int) xml_save_file_enc
      end function xml_save_file_enc
 
      ! FIXME:
@@ -57,40 +53,38 @@ module xml
 
      function xml_new_doc(version) &
           bind(c, name="xmlNewDoc")
-       use iso_c_binding, only: c_char, c_ptr
-       character(kind=c_char), intent(in) :: version (*)
-       type(c_ptr) :: xml_new_doc
+       use iso_c_binding, only: c_ptr
+       type(c_ptr), value :: version
+       type(c_ptr) xml_new_doc
      end function xml_new_doc
 
      function xml_new_node(namespace, name) &
           bind(c, name="xmlNewNode")
-       use iso_c_binding, only: c_char, c_ptr
-       type(c_ptr), value :: namespace
-       character(kind=c_char), intent(in) :: name (*)
-       type(c_ptr) :: xml_new_node
+       use iso_c_binding, only: c_ptr
+       type(c_ptr), value :: namespace, name
+       type(c_ptr) xml_new_node
      end function xml_new_node
 
      function xml_new_child(parent, namespace, name, content) &
           bind(c, name="xmlNewChild")
-       use iso_c_binding, only: c_char, c_ptr
-       type(c_ptr), value :: parent, namespace, content
-       character(kind=c_char), intent(in) :: name (*)
-       type(c_ptr) :: xml_new_child
+       use iso_c_binding, only: c_ptr
+       type(c_ptr), value :: parent, namespace, name, content
+       type(c_ptr) xml_new_child
      end function xml_new_child
 
      function xml_parse_memory(buffer, size) &
           bind(c, name="xmlParseMemory")
-       use iso_c_binding, only: c_char, c_int, c_ptr
-       type   (c_ptr), value :: buffer
+       use iso_c_binding, only: c_int, c_ptr
+       type(c_ptr), value :: buffer
        integer(c_int), value :: size
-       type   (c_ptr)        :: xml_parse_memory
+       type(c_ptr) xml_parse_memory
      end function xml_parse_memory
 
      function xml_set_root_element(doc, root) &
           bind(c, name="xmlDocSetRootElement")
        use iso_c_binding, only: c_ptr
        type(c_ptr), value :: doc, root
-       type(c_ptr) :: xml_set_root_element
+       type(c_ptr) xml_set_root_element
      end function xml_set_root_element
   end interface
 end module xml
--- tests/main.f90
+++ tests/main.f90
@@ -6,19 +6,20 @@ program main
   block
     use xml
 
-    character(:, c_char),   allocatable, target :: contents
-    character(:, c_char),   allocatable         :: data
-    type     (c_ptr)                            :: doc, node, cptr, root
-    character(kind=c_char), pointer             :: fptr (:)
-    integer  (c_int)                            :: size
+    character(:), allocatable, target :: contents, data, tag, version
+    type(c_ptr) doc, node, cptr, root
+    character, pointer :: fptr (:)
+    integer size
 
     contents = "contents" // c_null_char
 
-    doc = xml_new_doc("1.0" // c_null_char)
+    version = "1.0" // c_null_char
+    doc = xml_new_doc(c_loc(version))
 
-    root = xml_new_node(c_null_ptr, "root" // c_null_char)
-    node = xml_new_child( &
-         root, c_null_ptr, "node" // c_null_char, c_loc(contents))
+    tag = "root" // c_null_char
+    root = xml_new_node(c_null_ptr, c_loc(tag))
+    tag = "node" // c_null_char
+    node = xml_new_child(root, c_null_ptr, c_loc(tag), c_loc(contents))
     node = xml_set_root_element(doc, root)
 
     call xml_doc_dump_memory(doc, cptr, size)
